620e60a6c564e54b83df871d8c2d0110a7cbc38e
Ersin <ersin.metovski@gmail.com>
2017-10-25 23:16:00 +0200

    added read from console test

--- src/Hej.java
+++ src/Hej.java
@@ -1,8 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Hej {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Enter name:");
+		String name = null;
+
+		java.io.BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			name = in.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // if user not entered assumes default value.
+		System.out.println("Name= " + name);
 	}
 
 }
